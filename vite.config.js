93a495b1608b94c8bb19799c3939f24b9fcd9fb1
Alberto <albertopastrolin@gmail.com>
2021-11-20 21:51:57 +0100

    First commit

---
+++ vite.config.js
@@ -0,0 +1,75 @@
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import Unocss from 'unocss/vite'
+import Vue from '@vitejs/plugin-vue'
+import analyze from 'rollup-plugin-visualizer'
+import gzipPlugin from 'rollup-plugin-gzip'
+import path from 'path'
+import presetIcons from '@unocss/preset-icons'
+import { defineConfig } from 'vite'
+import { presetAttributify, presetUno } from 'unocss'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+    Vue({
+      refTransform: true,
+    }),
+    AutoImport({
+      imports: ['vue', 'vue-router', '@vueuse/core'],
+      dts: true,
+    }),
+    Components({
+      resolvers: [IconsResolver({ prefix: 'icon' })],
+      dts: true,
+    }),
+    Icons(),
+    Unocss({
+      shortcuts: [],
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons({
+          scale: 1.2,
+        }),
+      ],
+    }),
+  ],
+  server: {
+    fs: {
+      strict: true,
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        // GZIP compression as .gz files
+        gzipPlugin(),
+        // Brotil compression as .br files
+        gzipPlugin({
+          customCompression: (c) => brotliCompressSync(Buffer.from(c)),
+          fileName: '.br',
+        }),
+        analyze({
+          filename: 'dist/report.html',
+          gzipSize: true,
+          brotliSize: true,
+        }),
+      ],
+    },
+  },
+  optimizeDeps: {
+    include: [
+      'vue',
+      'vue-router',
+      '@vueuse/core',
+    ],
+  },
+})
