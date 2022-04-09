f5f44cde8537ea462f32541ad97ff44226953c60
Alberto <albertopastrolin@gmail.com>
2022-04-09 14:40:26 +0200

    chore: prettier

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import { createApp } from 'vue'
-import urql from '@urql/vue'
-import App from './App.vue'
-import 'uno.css'
+import { createApp } from "vue";
+import urql from "@urql/vue";
+import App from "./App.vue";
+import "uno.css";
 
 const app = createApp(App);
 
 app.use(urql, {
-  url: import.meta.env.VITE_GRAPHQL_ENDPOINT
-})
+  url: import.meta.env.VITE_GRAPHQL_ENDPOINT,
+});
 
-app.mount('#app')
+app.mount("#app");
